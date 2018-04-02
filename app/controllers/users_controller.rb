b7e1123eefb47342dd43fda92988dc87368fe036
t-kurihara <takuya.kurihara00@gmail.com>
2018-04-02 22:32:57 +0900

    [add] create user process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
+#
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(name: params[:name], mail: params[:mail], password: params[:password])
+
+    @result_message = 'success'
+  end
 end
