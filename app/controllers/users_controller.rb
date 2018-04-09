7af3f903a11f8bc9cbba391036fbab51a2bdf04e
t-kurihara <takuya.kurihara00@gmail.com>
2018-04-09 23:04:45 +0900

    [add] user registor confirm page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 #
 class UsersController < ApplicationController
+  attr_accessor :user
   def new
     @user = User.new
+  end
+
+  def confirm
+    @user = create_user(params[:name], params[:mail], params[:password])
   end
 
   def create
     @user = User.create(name: params[:name], mail: params[:mail], password: params[:password])
 
     @result_message = 'success'
+  end
+
+  private
+  def create_user name, mail, password
+    user = User.new
+    user.name = name
+    user.mail = mail
+    user.password = password
+    user
   end
 end
