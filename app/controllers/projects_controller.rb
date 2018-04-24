274c71880f484fadd9d0b1af89b27908e1d5a614
t-kurihara <takuya.kurihara00@gmail.com>
2018-04-24 23:48:33 +0900

    [add] project regist pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,34 @@
 class ProjectsController < ApplicationController
+  attr_accessor :project
+
+  def new
+    @project = Project.new
+  end
+
+  def confirm
+    @project = create_project params[:project]
+    p @project.to_s
+  end
+
+  def create
+    @project = Project.create(
+      name: params[:project][:name],
+      user_id: params[:project][:user_id],
+      about: params[:project][:about],
+      start_date: params[:project][:start_date],
+      end_date: params[:project][:end_date]
+    )
+
+    @result_message = 'success'
+  end
+
+  private
+  def create_project params
+    project = Project.new
+    project.name = params[:name]
+    project.about = params[:about]
+    project.start_date = params[:start_date]
+    project.end_date = params[:end_date]
+    project
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, :projects
   post '/users/confirm' => 'users#confirm'
+  post '/projects/confirm' => 'projects#confirm'
 end
