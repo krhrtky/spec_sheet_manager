89c025ed3e574be6153a80146ff96225053a71e5
t-kurihara <takuya.kurihara00@gmail.com>
2018-04-30 22:51:02 +0900

    [add] add login(index) page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, :projects
+  get '/' => 'index#index'
   post '/users/confirm' => 'users#confirm'
   post '/projects/confirm' => 'projects#confirm'
 end
